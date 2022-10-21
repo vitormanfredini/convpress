ed61a9fe6c327933b4e98e1e9dec04ed839d29c1
Vítor Manfredini <vitormanfredini@gmail.com>
2022-10-20 21:40:14 -0300

    README with instructions and default values in the help message

--- README.md
+++ README.md
@@ -7,6 +7,40 @@ It's slow and not recomended for any type of production use.
 
 ---
 
-Testes:
+To install, first create a virtual environment:
+
+`virtualenv venv_convpress`
+
+and activate it:
+
+`source venv_convpress/bin/activate`
+
+Install dependencies:
+
+`pip install -r requirements.txt`
+
+---
+
+To compress a file:
+
+`./convpress.py data/original-file.txt data/compressed-file.cp`
+
+To uncompress a file:
+
+`./deconvpress.py data/compressed-file.cp data/uncompressed-file.txt`
+
+---
+
+To see all parameters that you can pass to the compressing program:
+
+`./convpress.py -h`
+
+---
+
+You can recompress a file that has already been compressed and it will make it even smaller, though less and less until it can't compress it further.
+
+---
+
+Tests:
 
 `python -m unittest`
--- arguments.py
+++ arguments.py
@@ -12,22 +12,22 @@ def parse_args_compress():
     parser.add_argument('input_file', type=argparse.FileType('rb'))
     parser.add_argument('output_file', type=argparse.FileType('wb'))
     parser.add_argument('--ps', '--population_size', type=int,
-                        default=75, help='max number of filters in each generation'
+                        default=75, help='max number of filters in each generation (default is 75)'
                         )
     parser.add_argument('--g', '--generations', type=int,
-                        default=25, help='how many generations to run',
+                        default=25, help='how many generations to run (default is 25)',
                         )
     parser.add_argument('--fsmin', '--filter_size_min', type=int,
-                        default=2, help='minimum filter',
+                        default=2, help='minimum filter (default is 2)',
                         )
     parser.add_argument('--fsmax', '--filter_size_max', type=int,
-                        default=3, help='maximum filter',
+                        default=3, help='maximum filter (default is 3)',
                         )
     parser.add_argument('--mcp', '--mutation_chance_percentage', type=float,
-                        default=0.15, help='percentage of filters that will suffer mutation',
+                        default=0.15, help='percentage (between 0 and 1.0) of filters that will suffer mutation. ex: 0.33 (default is 0.15)',
                         )
     parser.add_argument('--scp', '--survival_chance_percentage', type=float,
-                        default=0.5, help='percentage of filters that survive in each generation (based on their scores).',
+                        default=0.5, help='percentage (between 0 and 1.0) of filters that survive in each generation (based on their scores). ex: 0.40 (default is 0.5)',
                         )
     return parser.parse_args()
 
