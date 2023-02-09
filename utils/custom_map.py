c37b5054f9b631c3605d5fd67ae5e72e90bbad3d
Vítor Manfredini <vitormanfredini@gmail.com>
2023-02-09 16:17:13 -0300

    comment

--- utils/custom_map.py
+++ utils/custom_map.py
@@ -8,7 +8,7 @@ def map_easein(percentage: float) -> float:
     Receive a value between 0 and 1
     and also returns a value between 0 and 1
     but mapped to a growing curve.
-    Values closer to zero get closer to zero
+    Values closer to zero get valuyes closer to zero
     and ramp up fast when it's closer to 1.
     """
     return math.cos(math.radians(-90 + percentage * 90))
